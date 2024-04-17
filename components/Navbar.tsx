fcc4b798d17cff8a0d05d3282d3228125467a515
Anand Panda <anand612p@gmail.com>
2024-04-17 23:24:59 +0530

    Home Page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
-import MobileNav from './MobileNav';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return (
-    <section className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
-      <Link href='/' className='flex items-center gap-1'>
-        <Image
-          src="icons/logo.svg"
-          width={32}
-          height={32}
-          alt="Boom Logo"
-          className='max-sm:size-10'
-        />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Boom</p>
-      </Link>
+	return (
+		<section className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+			<Link
+				href="/"
+				className="flex items-center gap-1"
+			>
+				<Image
+					src="icons/logo.svg"
+					width={32}
+					height={32}
+					alt="Boom Logo"
+					className="max-sm:size-10"
+				/>
+				<p className="text-[26px] font-extrabold text-white max-sm:hidden">
+					Boom
+				</p>
+			</Link>
 
-      <div className='flex-between gap-5'>
-        {/* Clerk User Management */}
+			<div className="flex-between gap-5">
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 
-        <MobileNav />
-      </div>
-    </section>
-  )
-}
+				<MobileNav />
+			</div>
+		</section>
+	);
+};
 
-export default Navbar
+export default Navbar;
